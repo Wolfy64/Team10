376ef00b01621bb00f4f3e17bf5721f8b3117564
David <ledavid64@gmail.com>
2018-08-19 07:01:39 -0700

    Add Goals Achieved/Missed fron Database

--- src/components/dashboard/goals/goals.js
+++ src/components/dashboard/goals/goals.js
@@ -1,95 +1,105 @@
-import React, {Component} from 'react';
-import {Footer, Header, Navigation} from '../../index';
-import {bindActionCreators} from 'redux';
+import React, { Component } from 'react';
+import { Footer, Header, Navigation } from '../../index';
+import { bindActionCreators } from 'redux';
 import * as userActions from '../../../actions/user';
 import connect from 'react-redux/es/connect/connect';
 import Goal from './goal';
 
 class Goals extends Component {
-    componentWillMount() {
-        if (!this.props.userReducer.isAuthorized || this.props.userReducer.profile === undefined) {
-            this.props.history.push('/signin');
-        }
-        if(this.props.userReducer.user === undefined){this.props.userActions.getUser(this.props.userReducer.profile.email);}
+  componentWillMount() {
+    if (
+      !this.props.userReducer.isAuthorized ||
+      this.props.userReducer.profile === undefined
+    ) {
+      this.props.history.push('/signin');
     }
-
-    componentWillUpdate(nextProps) {
-        if (!nextProps.userReducer.isAuthorized || nextProps.userReducer.profile === undefined) {
-            nextProps.history.push('/signin');
-        }
-        if(nextProps.userReducer.user === undefined){nextProps.userActions.getUser(nextProps.userReducer.profile.email);}
+    if (this.props.userReducer.user === undefined) {
+      this.props.userActions.getUser(this.props.userReducer.profile.email);
     }
+  }
 
-    handleGoalList() {
-        return this.props.userReducer.user.goals.map(goal => (
-            <Goal
-                key={goal.id}
-                id={goal.id}
-                isActive={goal.isActive}
-                name={goal.name}
-                category={goal.category}
-                achieved={goal.achieved}
-            />
-        ));
+  componentWillUpdate(nextProps) {
+    if (
+      !nextProps.userReducer.isAuthorized ||
+      nextProps.userReducer.profile === undefined
+    ) {
+      nextProps.history.push('/signin');
     }
+    if (nextProps.userReducer.user === undefined) {
+      nextProps.userActions.getUser(nextProps.userReducer.profile.email);
+    }
+  }
 
-    render() {
-        return (
-            <div className="wrapper">
-                <Navigation title="Goals"/>
+  handleGoalList() {
+    return this.props.userReducer.user.goals.map(goal => (
+      <Goal
+        key={goal.id}
+        id={goal.id}
+        isActive={goal.isActive}
+        name={goal.name}
+        category={goal.category}
+        achieved={goal.achieved}
+      />
+    ));
+  }
 
-                <div className="main-panel">
-                    <Header title="Goals"/>
+  render() {
+    return (
+      <div className="wrapper">
+        <Navigation title="Goals" />
 
-                    <div className="content">
-                        <div className="container-fluid">
-                            <div className="col-md-12">
-                                <div className="card">
-                                    {/*<div className="header">
+        <div className="main-panel">
+          <Header title="Goals" />
+
+          <div className="content">
+            <div className="container-fluid">
+              <div className="col-md-12">
+                <div className="card">
+                  {/*<div className="header">
                                         <h4 className="title">My Goals</h4>
                                         <p className="category">Here is a subtitle on my goals</p>
                                     </div>*/}
-                                    <div className="content table-responsive table-full-width">
-                                        <table className="table">
-                                            <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Name</th>
-                                                <th>Category</th>
-                                                <th className="goals-text-center">Status</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            {this.props.userReducer.user
-                                                ? this.handleGoalList()
-                                                : ''}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* {console.log(this.props.userReducer.users)} */}
-
-                    <Footer/>
+                  <div className="content table-responsive table-full-width">
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Name</th>
+                          <th>Category</th>
+                          <th className="goals-text-center">Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.props.userReducer.user
+                          ? this.handleGoalList()
+                          : ''}
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+          {/* {console.log(this.props.userReducer.users)} */}
+
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {userReducer: state.userReducer};
+  return { userReducer: state.userReducer };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        userActions: bindActionCreators(userActions, dispatch),
-    };
+  return {
+    userActions: bindActionCreators(userActions, dispatch)
+  };
 };
 
 export default (Goals = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Goals));
--- src/components/dashboard/progress/progress.js
+++ src/components/dashboard/progress/progress.js
@@ -1,149 +1,182 @@
-import React, {Component} from "react";
-import {Footer, Header, Navigation} from "../../index";
-import {bindActionCreators} from "redux";
-import * as userActions from "../../../actions/user";
-import connect from "react-redux/es/connect/connect";
-import Milestones from "./milestones";
-import {Link} from "react-router-dom";
+import React, { Component } from 'react';
+import { Footer, Header, Navigation } from '../../index';
+import { bindActionCreators } from 'redux';
+import * as userActions from '../../../actions/user';
+import connect from 'react-redux/es/connect/connect';
+import Milestones from './milestones';
+import { Link } from 'react-router-dom';
 
 class Progress extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            weeks: 4,
-            goal: 1000,
-            goals: [
-                {amount: 100, done: false},
-                {amount: 300, done: true},
-                {amount: 400, done: true, current: true},
-                {amount: 100, done: undefined},
-                {amount: 100, done: undefined},
-            ],
-            amount: 500,
-            progress: 0
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      coins: 0,
+      weeks: 4,
+      goal: 1000,
+      goals: [
+        { amount: 100, done: false },
+        { amount: 300, done: true },
+        { amount: 400, done: true, current: true },
+        { amount: 100, done: undefined },
+        { amount: 100, done: undefined }
+      ],
+      amount: 500,
+      progress: 0
+    };
+  }
+
+  componentDidMount() {
+    this.handleProgressBar();
+    console.log(this.props.userReducer.user);
+  }
+
+  componentWillMount() {
+    if (
+      !this.props.userReducer.isAuthorized ||
+      this.props.userReducer.profile === undefined
+    ) {
+      this.props.history.push('/signin');
     }
-
-    componentDidMount() {
-        this.handleProgressBar();
+    if (this.props.userReducer.user === undefined) {
+      this.props.userActions.getUser(this.props.userReducer.profile.email);
     }
+  }
 
-    componentWillMount() {
-        if (!this.props.userReducer.isAuthorized || this.props.userReducer.profile === undefined) {
-            this.props.history.push('/signin');
-        }
-        if(this.props.userReducer.user === undefined){this.props.userActions.getUser(this.props.userReducer.profile.email);}
+  componentWillUpdate(nextProps) {
+    if (
+      !nextProps.userReducer.isAuthorized ||
+      nextProps.userReducer.profile === undefined
+    ) {
+      nextProps.history.push('/signin');
     }
-
-    componentWillUpdate(nextProps) {
-        if (!nextProps.userReducer.isAuthorized || nextProps.userReducer.profile === undefined) {
-            nextProps.history.push('/signin');
-        }
-        if(nextProps.userReducer.user === undefined){nextProps.userActions.getUser(nextProps.userReducer.profile.email);}
+    if (nextProps.userReducer.user === undefined) {
+      nextProps.userActions.getUser(nextProps.userReducer.profile.email);
     }
+  }
 
-    handleProgressBar() {
-        const {goal, amount} = this.state;
-        const progress = amount * 100 / goal;
-        this.setState({progress});
-    }
+  handleProgressBar() {
+    const activeGoal = this.props.userReducer.user.goals.find(
+      goal => goal.isActive
+    );
+    const progress = (activeGoal.currentAmount * 100) / activeGoal.objective;
+    this.setState({ progress });
+  }
 
-    render() {
-        const {mainGoal, weeks, goals, amount} = this.state;
-        return (
-            <div className="wrapper">
-                <Navigation title="Progress"/>
+  handleGoalAchieved() {
+    return this.props.userReducer.user.goals.filter(goal => goal.achieved)
+      .length;
+  }
 
-                <div className="main-panel">
-                    <Header title="Progress"/>
+  handleGoalMissed() {
+    return this.props.userReducer.user.goals.filter(goal => !goal.achieved)
+      .length;
+  }
 
+  render() {
+    const { mainGoal, weeks, goals, amount } = this.state;
+    return (
+      <div className="wrapper">
+        <Navigation title="Progress" />
+
+        <div className="main-panel">
+          <Header title="Progress" />
+
+          <div className="content">
+            <div className="container-fluid">
+              <div className="row">
+                {/* === Goals === */}
+                <div className="col-lg-4 col-sm-6">
+                  <div className="card">
                     <div className="content">
-                        <div className="container-fluid">
-                            <div className="row">
-                                {/* === Goals === */}
-                                <div className="col-lg-4 col-sm-6">
-                                    <div className="card">
-                                        <div className="content">
-                                            <div className="row">
-                                                <div className="col-xs-5">
-                                                    <div className="icon-big icon-success text-center">
-                                                        <i className="fas fa-check-circle"/>
-                                                    </div>
-                                                </div>
-                                                <div className="col-xs-7">
-                                                    <div className="numbers">
-                                                        <p>Goals Achieved</p>
-                                                        2
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                {/* ===  === */}
-                                {/* === Goals === */}
-                                <div className="col-lg-4 col-sm-6">
-                                    <div className="card">
-                                        <div className="content">
-                                            <div className="row">
-                                                <div className="col-xs-5">
-                                                    <div className="icon-big icon-danger text-center">
-                                                        <i className="fas fa-times-circle text-danger"/>
-                                                    </div>
-                                                </div>
-                                                <div className="col-xs-7">
-                                                    <div className="numbers">
-                                                        <p>Goals Missed</p>
-                                                        1
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                {/* ===  === */}
-                            </div>
-                            <hr/>
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <div className="card">
-                                        <div className="header">
-                                            <h4 className="title">Active Goal</h4>
-                                            <p className="category">This is your current goal's progress.</p>
-                                        </div>
-                                        <div className="content">
-                                            <div className="progress">
-                                                <div className="progress-bar progress-bar-striped active"
-                                                     role="progressbar"
-                                                     style={{width: `${this.state.progress}%`}}>
-                                                    {this.state.progress}%
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <Milestones goals={goals} mainGoal={mainGoal} weeks={weeks} amount={amount}/>
-                                </div>
-                            </div>
-
+                      <div className="row">
+                        <div className="col-xs-5">
+                          <div className="icon-big icon-success text-center">
+                            <i className="fas fa-check-circle" />
+                          </div>
                         </div>
+                        <div className="col-xs-7">
+                          <div className="numbers">
+                            <p>Goals Achieved</p>
+                            {this.handleGoalAchieved()}
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <Footer/>
+                  </div>
                 </div>
+                {/* ===  === */}
+                {/* === Goals === */}
+                <div className="col-lg-4 col-sm-6">
+                  <div className="card">
+                    <div className="content">
+                      <div className="row">
+                        <div className="col-xs-5">
+                          <div className="icon-big icon-danger text-center">
+                            <i className="fas fa-times-circle text-danger" />
+                          </div>
+                        </div>
+                        <div className="col-xs-7">
+                          <div className="numbers">
+                            <p>Goals Missed</p>
+                            {this.handleGoalMissed()}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* ===  === */}
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="card">
+                    <div className="header">
+                      <h4 className="title">Active Goal</h4>
+                      <p className="category">
+                        This is your current goal's progress.
+                      </p>
+                    </div>
+                    <div className="content">
+                      <div className="progress">
+                        <div
+                          className="progress-bar progress-bar-striped active"
+                          role="progressbar"
+                          style={{ width: `${this.state.progress}%` }}
+                        >
+                          {this.state.progress}%
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <Milestones
+                    goals={goals}
+                    mainGoal={mainGoal}
+                    weeks={weeks}
+                    amount={amount}
+                  />
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {userReducer: state.userReducer};
+  return { userReducer: state.userReducer };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        userActions: bindActionCreators(userActions, dispatch)
-    };
+  return {
+    userActions: bindActionCreators(userActions, dispatch)
+  };
 };
 
-export default (Progress = connect(mapStateToProps, mapDispatchToProps)(
-    Progress
-));
+export default (Progress = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Progress));
